c5a85919f9fd3609dc955cdfcee5a6c056178415
Elias Villarreal <evilla41@stu.southtexascollege.edu>
2026-05-07 19:19:58 -0500

    fix code 2

--- src/data/migrations/0002_curly_eddie_brock.sql
+++ src/data/migrations/0002_curly_eddie_brock.sql
@@ -1,2 +1,3 @@
-ALTER TABLE projects ADD user_id integer NOT NULL DEFAULT 1 REFERENCES users(id);
-CREATE INDEX `idx_projects_user_id` ON `projects` (`user_id`);
+ALTER TABLE projects ADD user_id integer DEFAULT 1;
+
+CREATE INDEX idx_projects_user_id ON projects (user_id);
